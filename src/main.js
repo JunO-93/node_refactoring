8b5ffc238ecd602761c528b66a49db0f23bf1200
JunO-93 <dlwnsdh17@naver.com>
2025-12-30 17:56:14 +0900

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import { statement } from './chapter01/function/statement.js';
+import invoices from './chapter01/data/invoices.json';
+import plays from './chapter01/data/plays.json';
+
+// statement 함수 실행
+const invoice = invoices[0]; // 첫 번째 청구서 사용 (또는 원하는 인덱스)
+const result = statement(invoice, plays);
+console.log(result);
+
+// 또는 HTML에 표시하려면
+const app = document.getElementById('app');
+if (app) {
+    app.innerHTML = `<pre style="font-size:25px;">${result}</pre>`;
+}
